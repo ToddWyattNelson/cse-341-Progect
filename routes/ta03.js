b7c8d3657b42bf970584bc76734b4650968fb089
ToddWyattNelson <nel17052@byui.edu>
2020-05-06 16:50:22 -0600

    Team Activity

--- routes/ta03.js
+++ routes/ta03.js
@@ -1,14 +1,40 @@
 //TA03 PLACEHOLDER
+const https = require('https');
 const express = require('express');
 const router = express.Router();
 
-router.get('/',(req, res, next) => {
-    res.render('pages/ta03', { 
-        title: 'Team Activity 03', 
-        path: '/ta03', // For pug, EJS 
-        activeTA03: true, // For HBS
-        contentCSS: true, // For HBS
+let parsedData
+router.get('/', (req, res, next) => {
+
+    https.get("https://byui-cse.github.io/cse341-course/lesson03/items.json", (pizza) => {
+
+        data = [];
+        pizza.on("data", (chunk) => {
+            console.log(chunk);
+            data.push(chunk);
+        });
+
+        pizza.on("end", () => {
+
+            const parsedData = JSON.parse(Buffer.concat(data).toString());
+            console.log(parsedData);
+            res.render('pages/ta03', {
+                title: 'Team Activity 03',
+                path: '/ta03', // For pug, EJS 
+                activeTA03: true, // For HBS
+                contentCSS: true, // For HBS
+                data: parsedData
+            });
+        });
+
+
+    });
+
+    router.post("/search-item", (req, res, next) =>{
+         const search = req.body.search;
     });
 });
+
+
 
 module.exports = router;
